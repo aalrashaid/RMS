3ce4eed96fa5693cc25956087cce2566fa8fcf99
aalrashaid <a.alrashid@me.com>
2022-09-09 08:56:44 +0800

    fonts scss

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstarp/bootstrap.css') }} " media="all">
+    <!-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstarp/bootstrap.css') }} " media="all"> -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/app.css') }}" media="all">
 </head>
 
@@ -26,8 +26,7 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
 </body>
 
